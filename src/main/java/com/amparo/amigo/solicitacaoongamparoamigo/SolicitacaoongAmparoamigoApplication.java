75566da0d57e61ed7989a8d8ec751320fe92c085
Natalia Milani <natalia.milani@hotmail.com>
2021-11-13 16:52:01 -0300

    Subida repositorio

---
+++ src/main/java/com/amparo/amigo/solicitacaoongamparoamigo/SolicitacaoongAmparoamigoApplication.java
@@ -0,0 +1,13 @@
+package com.amparo.amigo.solicitacaoongamparoamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolicitacaoongAmparoamigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolicitacaoongAmparoamigoApplication.class, args);
+	}
+
+}
