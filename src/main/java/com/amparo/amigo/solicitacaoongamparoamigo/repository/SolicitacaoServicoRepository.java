8a2a768418840d1b0fab2f7a7405f3b89a3c0faf
natsilva <nataliajsilva2@outlook.com>
2022-01-05 00:42:18 -0300

    criando metodo para buscar solicitacao por voluntarioId

--- src/main/java/com/amparo/amigo/solicitacaoongamparoamigo/repository/SolicitacaoServicoRepository.java
+++ src/main/java/com/amparo/amigo/solicitacaoongamparoamigo/repository/SolicitacaoServicoRepository.java
@@ -3,9 +3,12 @@ package com.amparo.amigo.solicitacaoongamparoamigo.repository;
 import com.amparo.amigo.solicitacaoongamparoamigo.domain.SolicitacaoServico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SolicitacaoServicoRepository extends JpaRepository<SolicitacaoServico, Integer> {
 
     Optional<SolicitacaoServico> findBySolicitacaoId(Integer solicitacaoId);
+
+    List<SolicitacaoServico> findByVoluntarioId(Integer voluntarioId);
 }
--- src/main/java/com/amparo/amigo/solicitacaoongamparoamigo/service/SolicitacaoSevicoService.java
+++ src/main/java/com/amparo/amigo/solicitacaoongamparoamigo/service/SolicitacaoSevicoService.java
@@ -1,10 +1,11 @@
 package com.amparo.amigo.solicitacaoongamparoamigo.service;
 
 import com.amparo.amigo.solicitacaoongamparoamigo.domain.SolicitacaoServico;
-import com.amparo.amigo.solicitacaoongamparoamigo.exceptions.SolicitacaoServicoNotFoundException;
+import com.amparo.amigo.solicitacaoongamparoamigo.exceptions.SolicitacaoNotFoundException;
 import com.amparo.amigo.solicitacaoongamparoamigo.repository.SolicitacaoServicoRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -17,15 +18,27 @@ public class SolicitacaoSevicoService {
         return repository.save(solicitacao);
     }
 
-    public Optional<SolicitacaoServico> consultarServicoId(Integer solicitacaoId){
+    public Optional<SolicitacaoServico> consultarSolicitacaoBySolicitacaoId(Integer solicitacaoId){
 
         var consultaSolicitacaoServico = repository.findBySolicitacaoId(solicitacaoId).isPresent();
 
         if(!consultaSolicitacaoServico){
-            throw new SolicitacaoServicoNotFoundException("Não encontrada id da solicitacao: " + solicitacaoId);
+            throw new SolicitacaoNotFoundException("Não encontrada id da solicitacao: " + solicitacaoId);
         }
 
         return repository.findBySolicitacaoId(solicitacaoId);
+
+    }
+
+    public List<SolicitacaoServico> consultarSolicitacaoByVoluntarioId(Integer voluntarioId){
+
+        var consultaSolicitacao = repository.findByVoluntarioId(voluntarioId);
+
+        if(consultaSolicitacao.isEmpty()){
+            throw new SolicitacaoNotFoundException("Não encontrada solicitacao para esse voluntario Id: " + voluntarioId);
+        }
+
+        return repository.findByVoluntarioId(voluntarioId);
 
     }
 
